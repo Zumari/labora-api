43c986d0cd752269e74f762f8f988fc0a96727be
Zumari <fernanda_zunigaa@yahoo.com>
2023-05-03 18:47:44 -0600

    update

--- sql/create_items.sql
+++ sql/create_items.sql
@@ -5,6 +5,6 @@ CREATE TABLE items(
 	customer_name VARCHAR(255) NOT NULL,
 	order_date TIMESTAMP NOT NULL,
 	product VARCHAR(255) NOT NULL,
-	quantity INTEGER NOT NULL,
-	price NUMERIC NOT NULL
+	quantity INTEGER NOT NULL CHECK(quantity > 0), 
+    price NUMERIC NOT NULL CHECK(price >= 0)
 );
