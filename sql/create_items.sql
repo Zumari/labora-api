073b24c3eb09bb9ce90d0bb7173e0680fe679bd0
Zumari <fernanda_zunigaa@yahoo.com>
2023-05-03 18:16:23 -0600

    correcciones

--- sql/create_items.sql
+++ sql/create_items.sql
@@ -1,66 +1,10 @@
---
--- PostgreSQL database dump
---
+CREATE DATABASE labora-proyect-1;
 
--- Dumped from database version 15.2
--- Dumped by pg_dump version 15.2
-
--- Started on 2023-05-03 15:05:36
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- TOC entry 214 (class 1259 OID 16428)
--- Name: items; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.items (
-    id integer NOT NULL,
-    customer_name character varying(255) NOT NULL,
-    order_date date NOT NULL,
-    product character varying NOT NULL,
-    quantity integer NOT NULL,
-    price numeric NOT NULL
+CREATE TABLE items(
+	id SERIAL PRIMARY KEY,
+	customer_name VARCHAR(255) NOT NULL,
+	order_date TIMESTAMP NOT NULL,
+	product VARCHAR(255) NOT NULL,
+	quantity INTEGER NOT NULL,
+	price NUMERIC NOT NULL
 );
-
-
-ALTER TABLE public.items OWNER TO postgres;
-
---
--- TOC entry 3315 (class 0 OID 16428)
--- Dependencies: 214
--- Data for Name: items; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.items (id, customer_name, order_date, product, quantity, price) FROM stdin;
-\.
-
-
---
--- TOC entry 3172 (class 2606 OID 16434)
--- Name: items items_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.items
-    ADD CONSTRAINT items_pkey PRIMARY KEY (id);
-
-
--- Completed on 2023-05-03 15:05:36
-
---
--- PostgreSQL database dump complete
---
-
--- sql/insert_and_select.sql
+++ sql/insert_and_select.sql
@@ -1,11 +1,16 @@
+
+DELETE items;
+DROP TABLE items;
+
 INSERT INTO public.items(
-	id, customer_name, order_date, product, quantity, price)
-	VALUES (1, 'Samantha', '23/05/03 16:06:41.50', 'Memoria USB 1TB', 25, 150.00),
-			(2, 'Pedro', '23/05/03 16:06:43.50', 'Vino La Rosa', 2, 120.00),
-			(3, 'Selim', '23/05/03 16:07:41.50', 'Caja plástica', 205, 100.00),
-			(4, 'Gloria', '23/05/03 16:10:41.50', 'Cepillo dental', 1, 24.50),
-			(5, 'Alejandra', '23/05/03 16:16:00.50', 'Mouse', 2, 250.49)
+	customer_name, order_date, product, quantity, price)
+	VALUES ('Samantha', '23/05/03 16:06:41.50', 'Memoria USB 1TB', 25, 150.00),
+			('Pedro', '23/05/03 16:06:43.50', 'Vino La Rosa', 2, 120.00),
+			('Selim', '23/05/03 16:07:41.50', 'Caja plástica', 205, 100.00),
+			('Gloria', '23/05/03 16:10:41.50', 'Cepillo dental', 1, 24.50),
+			('Alejandra', '23/05/03 16:16:00.50', 'Mouse', 2, 250.49)
 ;
 	
 SELECT * FROM items
 WHERE quantity > 2 AND price > 50;
+
